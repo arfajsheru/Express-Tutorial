c93c0933bd3bb50dd8d1231d69236c0fc76bdb3b
arfajsheru <arfatsheru74@gmail.com>
2025-03-15 11:02:46 +0530

    server-static-file added

--- server-static-files/index.js
+++ server-static-files/index.js
@@ -2,14 +2,20 @@ import express from "express"
 
 const app = express();
 app.use(express.json())
-app.set('view engine', 'ejs')
 
+// using ejs in node js 
+app.set('view engine', 'ejs')
 app.get("/", (req,res) => {
     const userName = "Arfaj"
     const lastName = "Sheru"
     const collegename = "kirandevi saraf institute of complete learning"
     res.render('index', {userName, lastName, collegename})
 })
+
+
+// Express static file serving
+app.use('/public',express.static('public'))
+app.use('/images',express.static('images'))
 
 const PORT = 4000
 
