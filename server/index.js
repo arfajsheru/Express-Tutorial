6b53d5dae8d93d57219dca2c48892e8f03d32214
arfajsheru <arfatsheru74@gmail.com>
2025-03-15 10:46:13 +0530

    testing ejs

--- server/index.js
+++ server/index.js
@@ -2,12 +2,14 @@ import express from "express"
 
 const app = express();
 app.use(express.json())
+app.set('view engine', 'ejs')
 
 app.get("/", (req,res) => {
-    res.send("Welcom to / server");
+    const userName = "Arfaj"
+    const lastName = "Sheru"
+    const collegename = "kirandevi saraf institute of complete learning"
+    res.render('index', {userName, lastName, collegename})
 })
-
-
 
 const PORT = 4000
 
